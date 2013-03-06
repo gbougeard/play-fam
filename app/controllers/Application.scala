ef7027207b711c657b2226bcf8fc500f3c7b9a23
Greg Bougeard <gbougeard@gmail.com>
2013-03-06 22:12:23 +0100

    Use securesocial

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import _root_.securesocial.core.Authorization
+import _root_.securesocial.core.Identity
+
 import play.api._
 import play.api.mvc._
 import play.api.data._
@@ -7,12 +10,36 @@ import play.api.data.Forms._
 
 import views._
 
-object Application extends Controller {
 
+object Application extends Controller with securesocial.core.SecureSocial {
 
-  def home = Action {
-    val html = views.html.index("Hello")
-    Ok(html)
+  def index = SecuredAction { implicit request =>
+    Ok(views.html.index(request.user))
+  }
+
+  // a sample action using the new authorization hook
+  def onlyTwitter = SecuredAction(WithProvider("twitter")) { implicit request =>
+  //
+  //    Note: If you had a User class and returned an instance of it from UserService, this
+  //          is how you would convert Identity to your own class:
+  //
+  //    request.user match {
+  //      case user: User => // do whatever you need with your user class
+  //      case _ => // did not get a User instance, should not happen,log error/thow exception
+  //    }
+    Ok("You can see this because you logged in using Twitter")
+  }
+  // a sample action using the new authorization hook
+  def onlyGithub = SecuredAction(WithProvider("github")) { implicit request =>
+  //
+  //    Note: If you had a User class and returned an instance of it from UserService, this
+  //          is how you would convert Identity to your own class:
+  //
+  //    request.user match {
+  //      case user: User => // do whatever you need with your user class
+  //      case _ => // did not get a User instance, should not happen,log error/thow exception
+  //    }
+    Ok("You can see this because you logged in using Github")
   }
 
   // -- Javascript routing
@@ -26,4 +53,11 @@ object Application extends Controller {
       ).as("text/javascript")
   }
 
+}
+
+// An Authorization implementation that only authorizes uses that logged in using twitter
+case class WithProvider(provider: String) extends Authorization {
+  def isAuthorized(user: Identity) = {
+    user.id.providerId == provider
+  }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt._
 import play.Project._
+import sbt.Keys._
 
 object ApplicationBuild extends Build {
 
@@ -10,48 +11,46 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc
     //      ,anorm
-//    , "com.typesafe" % "slick_2.10" % "1.0.0-RC2"
-//    , "com.typesafe" % "play-slick_2.10" % "0.2.7-SNAPSHOT"
+    //    , "com.typesafe" % "slick_2.10" % "1.0.0-RC2"
+    //    , "com.typesafe" % "play-slick_2.10" % "0.2.7-SNAPSHOT"
     //      ,"com.h2database" % "h2" % "1.3.166"
     //      ,"org.xerial" % "sqlite-jdbc" % "3.6.20"
     //      ,"org.slf4j" % "slf4j-nop" % "1.6.4" // <- disables logging
 
     // enables logging
-    , "org.slf4j" % "slf4j-api" % "1.6.4"
-    , "ch.qos.logback" % "logback-classic" % "0.9.28"
-
+    , "org.slf4j" % "slf4j-api" % "1.7.2"
+    , "ch.qos.logback" % "logback-classic" % "1.0.9"
 
     // Other database drivers
     //    ,  "org.apache.derby" % "derby" % "10.6.1.0"
     //     , "org.hsqldb" % "hsqldb" % "2.0.0"
     //      ,"postgresql" % "postgresql" % "8.4-701.jdbc4"
-
     , "mysql" % "mysql-connector-java" % "5.1.23"
-    , "com.yammer.metrics" % "metrics-scala_2.9.1" % "2.1.5"
-    , "com.yammer.metrics" % "metrics-graphite" % "2.1.5"
+    // Metrics
+    , "nl.grons" % "metrics-scala_2.10" % "2.2.0"
+    //    , "com.yammer.metrics" % "metrics-scala_2.10" % "2.2.0"
+    //    , "com.yammer.metrics" % "metrics-graphite" % "2.2.0"
+
+    , "securesocial" %% "securesocial" % "master-SNAPSHOT"
 
     , "org.webjars" % "webjars-play" % "2.1.0"
     //    , "org.webjars" % "requirejs" % "2.1.1"
-    , "org.webjars" % "bootstrap" % "2.3.1"
-    //    , "org.webjars" % "bootstrap-datepicker" % "2.2.1"
-    , "org.webjars" % "momentjs" % "1.7.2"
     , "org.webjars" % "angularjs" % "1.1.3"
     , "org.webjars" % "angular-ui" % "0.3.2-1"
     , "org.webjars" % "angular-strap" % "0.6.6"
+    , "org.webjars" % "bootstrap" % "2.3.1"
     , "org.webjars" % "font-awesome" % "3.0.0"
-    , "org.webjars" % "jquery" % "1.9.0"
     , "org.webjars" % "jquery-ui" % "1.9.2"
+    , "org.webjars" % "momentjs" % "1.7.2"
     , "org.webjars" % "tinymce-jquery" % "3.4.9"
     , "org.webjars" % "famfamfam-flags" % "0.0"
-
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
     // Add your own project settings here
-//      ).dependsOn(RootProject( uri("git://github.com/gbougeard/play-slick.git") ))
-  ).dependsOn(RootProject( uri("git://github.com/freekh/play-slick.git") ))
-
-
+    //  ).dependsOn(RootProject( uri("git://github.com/gbougeard/play-slick.git") ))
+  ).dependsOn(RootProject(uri("git://github.com/freekh/play-slick.git")))
 
 }
