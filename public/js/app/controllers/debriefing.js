9eaa89cf69334f33c103220b43e410669fd24110
Greg Bougeard <gbougeard@gmail.com>
2013-04-12 00:21:24 +0200

    Debriefing - Start ajax calls

--- public/js/app/controllers/debriefing.js
+++ public/js/app/controllers/debriefing.js
@@ -6,5 +6,32 @@ function DebriefingCtrl($scope, $http, $location) {
     $scope.idMatch = $location.absUrl().substring($location.absUrl().lastIndexOf('/') + 1);
     console.log($scope.idMatch);
 
+    $scope.loadMatch = function(idMatch){
+        jsRoutes.controllers.Matchs.jsonById(idMatch).ajax(
+            {
+                success: function (data, status) {
+                    console.log(data, status);
+                    $scope.loadEvent(data.eventId);
+                },
+                error: function (data, status) {
+                    console.error(data, status);
+                }
+            }
+        );
+    };
 
+    $scope.loadEvent = function(idEvent){
+        jsRoutes.controllers.Events.jsonById(idEvent).ajax(
+            {
+                success: function (data, status) {
+                    console.log(data, status);
+                },
+                error: function (data, status) {
+                    console.error(data, status);
+                }
+            }
+        );
+    };
+
+    $scope.loadMatch($scope.idMatch);
 }
