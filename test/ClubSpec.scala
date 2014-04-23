8dae83f804d22355dea38211e017e1fa8eed59e0
Greg Bougeard <gbougeard@gmail.com>
2014-04-23 22:39:04 +0200

    ClubSpec - insert club an retrieve as json

--- test/ClubSpec.scala
+++ test/ClubSpec.scala
@@ -6,7 +6,8 @@ import play.api.libs.json._
 import org.scalacheck.Arbitrary._
 import org.scalacheck.{Arbitrary, Gen}
 import org.specs2.ScalaCheck
-import play.api.test.PlaySpecification
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.{FakeHeaders, FakeRequest, WithApplication}
 
 
 /**
@@ -15,14 +16,41 @@ import play.api.test.PlaySpecification
  * For more information, consult the wiki.
  */
 
-class ClubSpec extends PlaySpecification with ScalaCheck with ClubGen {
+class ClubSpec extends FamSpecification with ScalaCheck with ClubGen {
 
-  "json from(to) iso" ! prop {
+  "json - from(to) iso" ! prop {
     (p: Club) =>
-//          println(s"p: $p")
-//          println(s"toJson: ${Json.toJson(p)}")
-//          println(s"from(to): ${Json.fromJson(Json.toJson(p))}")
+    //          println(s"p: $p")
+    //          println(s"toJson: ${Json.toJson(p)}")
+    //          println(s"from(to): ${Json.fromJson(Json.toJson(p))}")
       Json.fromJson(Json.toJson(p)) == JsSuccess(p)
+  }
+
+  "json - insert a club through DAO and retrieve it as json" in new WithApplication(fakeAppMemDBMinimal) {
+
+    val club = genClub.sample.get.copy(id = Some(1))
+//    println(s"club $club")
+    val clubJson = Json.toJson(club)
+//    val postRequest = FakeRequest(
+//      method = "POST",
+//      uri = "/clubs",
+//      headers = FakeHeaders(
+//        Seq("Content-type" -> Seq("application/json"))
+//      ),
+//      body = clubJson
+//    )
+//    val Some(result) = route(postRequest)
+//    status(result) must equalTo(OK)
+    models.Clubs.insert(club)
+    
+    val jsonHeaders = FakeHeaders(Seq("Accept" -> Seq("application/json")))
+    val getRequest = FakeRequest(method = "GET", uri = "/clubs/1", headers = jsonHeaders, body = AnyContentAsEmpty)
+
+    val clubs = route(getRequest).get
+
+    status(clubs) must equalTo(OK)
+    contentType(clubs) must beSome.which(_ == "application/json")
+    contentAsString(clubs) must beEqualTo(clubJson.toString())
   }
 }
 
